a9fea5902a0ccd378d4d8a40a53cb2d8d6ce2813
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-06-24 13:52:30 -0300

    feat: adicionando criação de tabela para caso não tenha tabela criada no banco de dados

--- backend/src/models/UserModel.ts
+++ backend/src/models/UserModel.ts
@@ -1,34 +1,51 @@
 import { pool } from "../database/db";
 
 class UserModel {
-    async createUser(email: string, password: string): Promise<void> {
-        const query = "INSERT INTO users (email, password) VALUES ($1, $2)";
-        const values = [email, password];
+  async createTable() {
+    const query = `
+            CREATE TABLE IF NOT EXISTS users (
+                id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                email TEXT UNIQUE NOT NULL,
+                password TEXT NOT NULL
+            )
+        `;
+    await pool.query(query);
+  }
 
-        try {
-            await pool.query(query, values);
-            console.log("Usuário criado no banco de dados.");
-        } catch (error) {
-            console.error("Erro ao inserir usuário:", error);
-            throw error;
-        }
+  async createUser(email: string, password: string): Promise<void> {
+    await this.createTable();
+
+    const query = "INSERT INTO users (email, password) VALUES ($1, $2)";
+    const values = [email, password];
+
+    try {
+      await pool.query(query, values);
+      console.log("Usuário criado no banco de dados.");
+    } catch (error) {
+      console.error("Erro ao inserir usuário:", error);
+      throw error;
     }
+  }
 
-    async findUserByEmail(email: string): Promise<{ id: number; email: string; password: string } | null> {
-        const query = "SELECT id, email, password FROM users WHERE email = $1";
-        const values = [email];
+  async findUserByEmail(
+    email: string
+  ): Promise<{ id: number; email: string; password: string } | null> {
+    await this.createTable();
 
-        try {
-            const result = await pool.query(query, values);
-            if (result.rows.length > 0) {
-                return result.rows[0];
-            }
-            return null;
-        } catch (error) {
-            console.error("Erro ao buscar usuário:", error);
-            throw error;
-        }
+    const query = "SELECT id, email, password FROM users WHERE email = $1";
+    const values = [email];
+
+    try {
+      const result = await pool.query(query, values);
+      if (result.rows.length > 0) {
+        return result.rows[0];
+      }
+      return null;
+    } catch (error) {
+      console.error("Erro ao buscar usuário:", error);
+      throw error;
     }
+  }
 }
 
 export default new UserModel();
