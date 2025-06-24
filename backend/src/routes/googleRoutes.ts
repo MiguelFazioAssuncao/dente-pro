d61bb57587996b04f30fe0dd4ffa73b80da3c2a3
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-06-24 14:48:57 -0300

    feat: criando implementação para fazer login/cadastro com google

---
+++ backend/src/routes/googleRoutes.ts
@@ -0,0 +1,14 @@
+import { handleGoogleCallback, redirectGoogle } from 'controllers/AuthGoogleController';
+import { Router } from 'express';
+
+const router = Router();
+
+router.get("google", async (req, res) => {
+    await redirectGoogle(req, res);
+});
+
+router.get("/google/callback", async (req, res) => {
+    await handleGoogleCallback(req, res);
+});
+
+export default router;
